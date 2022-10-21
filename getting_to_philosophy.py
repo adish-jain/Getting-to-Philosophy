21cd6b4a33dcc4efa87ed0c8b543133a7f0e561b
Adish Jain <adish@Adishs-MBP-2.lan>
2022-10-21 13:04:57 -0700

    working

--- getting_to_philosophy.py
+++ getting_to_philosophy.py
@@ -1,54 +1,40 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString, Tag
 import requests
 import sys
-import re
 
-# the issue right now is that when we remove the parantheses we remove the ability to make it a beautifulsoup again which makes it hard to access the first link
-# but accessing the first link without removing the parentheses results in the wrong first link
-# ideal state: remove parentheses without breaking the html so that you can still access the first link by making it a beautifulsoup again
 def get_to_philosophy(startingUrl):
     MAX_HOPS, TARGET, counter = 100, "Philosophy", 0
     response = requests.get(startingUrl)
     webpage = BeautifulSoup(response.content, 'html5lib')
-    header = webpage.find(id='firstHeading').span.text 
+    header = webpage.find(id='firstHeading').span.text # Header of the article
     while header != TARGET and counter < MAX_HOPS:
         content = webpage.find(
             id='mw-content-text').find(class_="mw-parser-output")
-        firstParagraph = None
-
+        nextUrl = None
+        stack = []
         for paragraph in content.find_all("p", recursive=False):
-            paragraphText = remove_text_between_parens(str(paragraph))
-            # paragraph = BeautifulSoup(paragraphText, 'html5lib')
-            print(paragraphText)
-            # print(type(paragraph))
-            # alink = paragraph.find("a", recursive=False)
-            # print(alink)
-            print("")
-            if "href" in paragraphText:
-                firstParagraph = paragraphText
+            if nextUrl:
                 break
-        # paragraphText = str(paragraph)
-        # print(firstParagraph)
-        match = re.search(r'href=[\'"]?([^\'" >]+)', firstParagraph)
-        print(match)
-        nextWiki = match.group(1)
-        nextUrl = f'https://en.wikipedia.org{nextWiki}'
-        print(nextUrl)
+            for element in paragraph.children:
+                if isinstance(element, NavigableString):
+                    [stack.append("(") for i in range(element.count("("))]
+                    [stack.pop() for i in range(element.count(")"))]
+                 
+                if isinstance(element, Tag) and (element.name == "a" or ((element.name == "b" or element.name == "i") and element.find("a"))) and not stack:
+                    if (element.name == "b" or element.name == "i"):
+                        element = element.find("a")
+                    nextWiki = element.get("href")
+                    if "/wiki/" not in nextWiki:
+                        continue
+                    nextUrl = f'https://en.wikipedia.org{nextWiki}'
+                    print(nextUrl)
+                    break
+
         response = requests.get(nextUrl)
         webpage = BeautifulSoup(response.content, 'html5lib')
-        header = webpage.find(id='firstHeading').span.text  # Header of the article
+        header = webpage.find(id='firstHeading').span.text  
         counter += 1
-
-# def find_next_link(startingUrl):
-
-
-def remove_text_between_parens(text):
-    n = 1  # run at least once
-    while n:
-        # remove non-nested/flat balanced parts
-        text, n = re.subn(r'\([^()]*\)', '', text)
-    return text
-
+    print(f"MAX_HOPS of {MAX_HOPS} reached") if counter == MAX_HOPS else print(str(counter) + " hops")
 
 if __name__ == '__main__':
     startingUrl = sys.argv[1]
